866b405369f1b8e6884d0b910fc4f4e33a6743cd
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-23 13:27:11 -0300

    Resolucao exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+function exercicio1 (nome, anoNascido, anoAtual) {
+    const maiorDeidade = (anoAtual - anoNascido >=18)
+
+    return  '${nome} é maior de idade? ${maiorDeidade}'
+
+}
+console.log(exercico1 ("lucas", "1997", "2022"))
+console.log(exercicio1 ("felipe", "2000", "2022"))
+
+
+
+
+
+
+
+/*# Exercício 1
+
+a) Declare uma função que imprima `Olá, [SEU NOME]!`. Chame esta função.
+
+b) Declare uma função que receba um número como parâmetro imprima no console a tabuada do dele. Chame esta função várias vezes com números diferentes.
+
+c) Comente as funções criadas acima, e reescreva-as utilizando expressões de função, ou __arrow functions__ 
+/*
