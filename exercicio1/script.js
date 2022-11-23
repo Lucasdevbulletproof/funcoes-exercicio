f611e8abdf778b21b57b6fb97862519f03d2522f
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-23 13:43:31 -0300

    resolução corrigida

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,11 +1,11 @@
 function exercicio1 (nome, anoNascido, anoAtual) {
     const maiorDeidade = (anoAtual - anoNascido >=18)
 
-    return  '${nome} é maior de idade? ${maiorDeidade}'
+    return  `${nome} é maior de idade? ${maiorDeidade}`
 
 }
-console.log(exercico1 ("lucas", "1997", "2022"))
-console.log(exercicio1 ("felipe", "2000", "2022"))
+console.log(exercicio1 ("lucas", "2005", "2022"))
+console.log(exercicio1 ("felipe", "2005", "2022"))
 
 
 
@@ -13,11 +13,11 @@ console.log(exercicio1 ("felipe", "2000", "2022"))
 
 
 
-/*# Exercício 1
+//# Exercício 1
 
-a) Declare uma função que imprima `Olá, [SEU NOME]!`. Chame esta função.
+//a) Declare uma função que imprima `Olá, [SEU NOME]!`. Chame esta função.
 
-b) Declare uma função que receba um número como parâmetro imprima no console a tabuada do dele. Chame esta função várias vezes com números diferentes.
+//b) Declare uma função que receba um número como parâmetro imprima no console a tabuada do dele. Chame esta função várias vezes com números diferentes.
 
-c) Comente as funções criadas acima, e reescreva-as utilizando expressões de função, ou __arrow functions__ 
-/*
+//c) Comente as funções criadas acima, e reescreva-as utilizando expressões de função, ou __arrow functions__ 
+//
